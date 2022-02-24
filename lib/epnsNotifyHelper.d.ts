7ff941acd07d7d928d444c3cbfde54c59b7cc9e2
xander <shuaibualexander@gmail.com>
2022-02-24 08:55:55 +0100

    build files

---
+++ lib/epnsNotifyHelper.d.ts
@@ -0,0 +1,14 @@
+import { ethers } from 'ethers';
+import { EPNSSettings } from '.';
+declare const _default: {
+    sendOffchainNotification: (communicatorDetails: EPNSSettings, payload: any, channelPrivateKey: any, recipientAddr: any, channelAddress: any) => Promise<any>;
+    uploadToIPFS: (payload: any, logger: any, ipfsGateway: any, simulate: any) => Promise<unknown>;
+    getInteractableContracts: (network: any, apiKeys: any, walletPK: any, deployedContract: any, deployedContractABI: any) => {
+        provider: ethers.providers.BaseProvider;
+        contract: ethers.Contract;
+        signingContract: ethers.Contract | null;
+    };
+    sendNotification: (signingContract: any, channel: any, recipientAddr: any, notificationType: any, notificationStorageType: any, notificationStoragePointer: any, waitForTx: any, logger: any, simulate: any) => Promise<unknown>;
+    preparePayload: (recipientAddr: any, payloadType: any, title: any, body: any, payloadTitle: any, payloadMsg: any, payloadCTA: any, payloadImg: any) => Promise<unknown>;
+};
+export default _default;
